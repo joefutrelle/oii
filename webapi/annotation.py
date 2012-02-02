a91f7dedf8d656b2c4ea8558800c051f24875442
joefutrelle <jfutrelle@whoi.edu>
2012-02-02 22:26:07 +0000

    first web service! see https://beagle.whoi.edu/redmine/issues/947

--- webapi/annotation.py
+++ webapi/annotation.py
@@ -1,6 +1,8 @@
 from flask import Flask
-import os
 from unittest import TestCase
+from oii.utils import gen_id
+import json
+import re
 
 """Prototype annotation web API
 see https://beagle.whoi.edu/redmine/issues/948
@@ -12,6 +14,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/generate_ids/<int:n>')
+def generate_ids(n):
+    return json.dumps([gen_id() for _ in range(n)])
+    
 class TestApi(TestCase):
     def setUp(self):
         self.app = app.test_client()
@@ -21,3 +27,9 @@ class TestApi(TestCase):
 class TestAnnotation(TestApi):
     def test_hw(self):
         assert self.app.get('/').data == 'Hello World!'
+    def test_gen_ids(self):
+        ids = json.loads(self.app.get('/generate_ids/20').data)
+        assert len(ids) == 20
+        for id in ids:
+            assert len(id) == 40
+            assert re.match('[a-f0-9]+',id)
