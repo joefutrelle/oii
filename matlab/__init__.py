f87163485fba3c9214810b94cea6f1b14fb062df
joefutrelle <jfutrelle@whoi.edu>
2012-04-09 21:43:51 +0000

    working on capturing output and return code ... still seeing an issue where successful completion is not apparently returning zero as a return code.

--- matlab/__init__.py
+++ matlab/__init__.py
@@ -9,14 +9,31 @@ class Matlab:
         self.output_separator = '__BEGIN MATLAB OUTPUT__'
     def run(self,command):
         env = dict(MATLABPATH=':'.join(self.matlab_path))
+        p = None
         try:
-            cmd = '%s -nodisplay -r "try, disp(\'%s\'), %s, catch, exit(1), end, exit(0)"' % (self.exec_path, self.output_separator, command)
-            p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,env=env)
-            out, _ = p.communicate()
+            cmd = [
+                self.exec_path,
+                '-nodisplay',
+                '-r',
+                'try, disp(\'%s\'), %s, catch, exit(1), end, exit(0)' % (self.output_separator, command)
+                ]
+            p = subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,env=env)
+            seen_separator = False
+            while p.poll() is None:
+                while True:
+                    line = p.stdout.readline()
+                    if not line:
+                        break
+                    line = line.rstrip()
+                    if seen_separator:
+                        yield line
+                    elif not seen_separator and line == self.output_separator:
+                        seen_separator = True
             if p.returncode != 0 and self.fail_fast:
-                raise
-            _, content = re.split(self.output_separator,out)
-            return re.split('\n',content.strip())
+                print 'return code is %d' % p.returncode
+                raise RuntimeError('nonzero return code from subprocess: %d' % p.returncode)
+        except KeyboardInterrupt:
+            p.kill()
         except:
             if self.fail_fast:
                 raise
