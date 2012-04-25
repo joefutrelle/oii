f650d25665b191ade967ea82d50f1e75ac1efa0a
joefutrelle <jfutrelle@whoi.edu>
2012-04-25 17:28:29 +0000

    output callback can now terminate job by raising exception. see #1207

--- matlab/__init__.py
+++ matlab/__init__.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import subprocess
 import re
 from oii.times import iso8601
@@ -7,20 +8,21 @@ def reset_tty():
     subprocess.call(['stty','echo'])
 
 def message(msg='WARNING'):
-    return ' '.join(iso8601(),msg)
+    return ' '.join([iso8601(),str(msg)])
 
 class Matlab:
-    def __init__(self,exec_path,matlab_path=['.'],fail_fast=True,output_callback=lambda _: None):
+    def __init__(self,exec_path,matlab_path=['.'],fail_fast=True,output_callback=lambda _: None,log_callback=lambda x: print(x)):
         self.exec_path = exec_path
         self.matlab_path = matlab_path
         self.fail_fast = fail_fast
         self.output_separator = '__BEGIN MATLAB OUTPUT__'
         self.output_callback = output_callback
+        self.log_callback = lambda x: log_callback(message(x))
     def run(self,command):
         env = dict(MATLABPATH=':'.join(self.matlab_path))
         p = None
         try:
-            script = 'try, disp(\'%s\'), %s, catch err, disp(err.identifier), exit(1), end, exit(0)' % (self.output_separator, command)
+            script = 'try, disp(\'%s\'), %s, catch err, disp(err.message), disp(err.stack), disp(err.identifier), exit(1), end, exit(0)' % (self.output_separator, command)
             cmd = [
                 self.exec_path,
                 '-nodisplay',
@@ -36,17 +38,22 @@ class Matlab:
                         break
                     line = line.rstrip()
                     if seen_separator:
-                        self.output_callback(line)
+                        try:
+                            self.output_callback(line)
+                        except:
+                            self.log_callback('Output callback raised exception; killing Matlab process %d' % p.pid)
+                            reset_tty()
+                            p.kill()
+                            raise
                     elif not seen_separator and line == self.output_separator:
                         seen_separator = True
             if p.returncode != 0 and self.fail_fast:
-                print 'return code is %d' % p.returncode
-                raise RuntimeError('nonzero return code from subprocess: %d' % p.returncode)
-            reset_tty()
+                self.log_callback('Matlab return code is %d' % p.returncode)
+                raise RuntimeError('Nonzero return code (%d) from Matlab process %d' % (p.pid, p.returncode))
         except KeyboardInterrupt:
-            print 'killing matlab process %d' % p.pid
-            p.kill()
+            self.log_callback('Keyboard interrupt; killing Matlab process %d' % p.pid)
             reset_tty()
+            p.kill()
             raise
         except:
             reset_tty()
