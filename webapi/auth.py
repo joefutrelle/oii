59880f30a1d0b5efb170dac0f47395cb55de0438
joefutrelle <jfutrelle@whoi.edu>
2012-06-06 16:37:06 +0000

    now supports logout

--- webapi/auth.py
+++ webapi/auth.py
@@ -7,17 +7,22 @@ import json
 # all the same thing: "auth". So we have three names:
 # module: auth
 # blueprint: auth_api
-# function: authenticate
-# so the url_for call is url_for('auth_api.authenticate')
+# function: login, logout
+# so the url_for call is url_for('auth_api.login')
 
 auth_api = Blueprint('auth_api', __name__)
 auth_api.auth_callback = lambda u,p: True
 
-@auth_api.route('/authenticate',methods=['POST'])
-def authenticate():
+@auth_api.route('/login',methods=['POST'])
+def login():
     (u,p) = (request.form['username'], request.form['password'])
     if auth_api.auth_callback(u,p):
         session['username'] = u
         return json.dumps(dict(username=u))
     else:
         abort(401)
+
+@auth_api.route('/logout')
+def logout():
+    session.pop('username', None)
+    return json.dumps(dict(status='loggedOut'))
--- webapi/static/auth.js
+++ webapi/static/auth.js
@@ -1,12 +1,13 @@
 auth = {};
 auth.challenge = function(element, callback) {
-    $(element).append('Username: <input id="auth_username"> Password: <input type="password" id="auth_password"> <a href="#" id="auth_login" class="button">Login</a>')
+    $(element).empty()
+	.append('Username: <input id="auth_username"> Password: <input type="password" id="auth_password"> <a href="#" id="auth_login" class="button">Login</a>')
 	.find('.button').button()
 	.end()
 	.find('#auth_login')
 	.click(function() {
 	    $.ajax({
-		url: '/authenticate',
+		url: '/login',
 		type: 'POST',
 		dataType: 'json',
 		data: {
@@ -15,6 +16,14 @@ auth.challenge = function(element, callback) {
 		},
 		success: function(data) {
 		    callback(data.username);
+		    $(element).empty()
+			.append('Logged in as '+data.username+' <a href="#" id="auth_logout" class="button">Logout</a>')
+			.find('.button').button()
+			.end()
+			.find('#auth_logout').click(function () {
+			    $.getJSON('/logout');
+			    auth.challenge(element, callback);
+			});
 		},
 		failure: function() {
 		    alert('wrong credentials. no cookie for you!');
