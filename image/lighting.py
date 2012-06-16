6988f7225c178b33a864fe416d6eb9046187e762
joefutrelle <jfutrelle@whoi.edu>
2012-06-16 13:46:48 +0000

    scribbling and bibbling

--- image/lighting.py
+++ image/lighting.py
@@ -9,9 +9,9 @@ def estimate_lightfield(img):
     # FIXME hardcoded params probably tuned for 5-8MP images with features of a certain size
     (w,h) = img.size;
     bg = img.resize((100,100), Image.BICUBIC)
-    for i in range(5):
+    for i in range(5): # recursive median tends to delete objects
         bg = bg.filter(ImageFilter.MedianFilter(21));
-    bg = bg.resize((w,h)).filter(ImageFilter.SMOOTH_MORE)
+    bg = bg.resize((w,h))
     return bg
 
 def lightfield_correct(img,lightfield=None):
@@ -21,7 +21,7 @@ def lightfield_correct(img,lightfield=None):
     if lightfield is None:
         lightfield = estimate_lightfield(img)
     # FIXME hardcoded params probably tuned to certain images
-    gray = ImageEnhance.Color(lightfield).enhance(0.3)
-    hicontrast = ImageEnhance.Contrast(gray).enhance(2)
+    gray = ImageEnhance.Color(lightfield).enhance(0.25)
+    hicontrast = ImageEnhance.Contrast(gray).enhance(2.25)
     darkened = ImageEnhance.Brightness(hicontrast).enhance(0.5)
     return ImageEnhance.Brightness(ImageChops.subtract(img, darkened)).enhance(1.5)
