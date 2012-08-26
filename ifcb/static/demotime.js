28d53738487e4d161d1d8405bcdc613ded5084fc
joefutrelle <jfutrelle@whoi.edu>
2012-08-26 00:06:40 +0000

    factored IFCB stuff out of timeline plugin.

--- ifcb/static/demotime.js
+++ ifcb/static/demotime.js
@@ -1,36 +1,78 @@
 $(document).ready(function() {
+    // add a title
     var title = 'MVCO data volume';
-    var cache = {};
-    function latest_bin(date, callback) {
-	var ds = date.toISOString();
-	var pid = cache[ds];
-	if(pid == undefined) {
-	    $.getJSON('/api/feed/nearest/'+ds, function(r) {
-		pid = r.pid;
-		cache[ds] = pid;
-		callback(pid);
-	    });
-	} else {
-	    callback(pid);
-	}
-    }
     $('body').append('<h1>'+title+'</h1>');
-    $('body').append('<div></div>').find('div').timeline()
-	.bind('dateClick', function(event, date) {
-	    latest_bin(date, function(pid) {
+    // add the timeline control
+    $('body').append('<div id="timeline"></div>').find('div').timeline()
+	.bind('dateHover', function(event, date, clientX) { // on hover, show the date
+	    $('#date_label').empty().append(date.toString());
+	    $('#date_label').css('margin-left',clientX);
+	})
+	.bind('dateClick', function(event, date) { // on click,
+	    var ds = date.toISOString();
+	    $.getJSON('/api/feed/nearest/'+ds, function(r) { // find the nearest bin
+		// now draw a multi-page mosaic
+		var pid = r.pid;
+		var roi_scale = 0.333; // scaling factor per roi
+		var width = 600; // width of displayed mosaic
+		var height = 480; // height of displayed mosaic
+		// put 30 pages on the pager, just in case it's a huge bin
 		var images = []
-		var mosaic_scale = 1.0;
-		var roi_scale = 0.333 * mosaic_scale;
-		var width = 600;
-		var height = 480;
-		var s_width = Math.floor(width * mosaic_scale);
-		var s_height = Math.floor(height * mosaic_scale);
 		for(page=1; page <= 30; page++) {
-		    images.push('/api/mosaic/size/'+s_width+'x'+s_height+'/scale/'+roi_scale+'/page/'+page+'/pid/'+pid+'.jpg');
+		    images.push('/api/mosaic/size/'+width+'x'+height+'/scale/'+roi_scale+'/page/'+page+'/pid/'+pid+'.jpg');
 		}
+		// create an image pager
 		$('#mosaic_pager').empty().append('<div/><p class="bin_label">'+pid+'</p>')
-		    .find('div:last').imagePager(images, s_width, s_height);
+		    .find('div:last').imagePager(images, width, height);
 	    });
 	});
+    // now load the data volume series
+    console.log('loading data series...');
+    $.getJSON('/api/volume', function(volume) {
+	// make a bar graph showing data volume
+	var data = [];
+	$.each(volume, function(ix, day_volume) {
+	    // for each day/volume record, make a bar
+	    // get the volume data for that day
+	    var bin_count = day_volume.bin_count;
+	    var date = day_volume.day; // FIXME key should be "date"
+	    var gb = day_volume.gb; // FIXME currently ignored
+	    // create a one-day time interval
+	    var year = date.split('-')[0];
+	    var month = date.split('-')[1];
+	    var day = date.split('-')[2];
+	    var start = new Date(year, month-1, day)
+	    var end = new Date(year, month-1, day);
+	    end.setHours(end.getHours() + 24);
+	    // create a bar. here we need to scale GB to > 1px/GB or else the bars'll be tiny
+	    var height = Math.round(gb * 15);
+	    var style = 'height:' + height + 'px;'
+	    var color = '#ff0000';
+	    style = 'height:' + height + 'px;' +
+		'background-color: ' + color + ';'+
+		'border: 1px solid ' + color + ';';
+	    var bar = '<div class="bar" style="' + style + '" ' +
+		' title="'+gb+'GB"></div>'; // FIXME remove insignificant digits
+	    var item = {
+		'group': 'Data volume', // "Group" is displayed on the left as a label
+		'start': start,
+		'end': end,
+		'content': bar
+	    };
+	    // we're done with one bar
+	    data.push(item);
+	});
+	// layout parameters accepted by showdata and passed to underlying widget
+	var timeline_options = {
+	    "width":  "100%",
+	    "height": "200px",
+	    "style": "box"
+	};
+	// now tell the timeline plugin to draw it
+	$('#timeline').trigger('showdata', [data, timeline_options]);
+    });
+    // our date label goes below the timeline
+    $('body').append('<div id="date_label"></div>');
+    // and the mosaic pager is below that
     $('body').append('<div id="mosaic_pager"></div>');
 });
--- ifcb/static/jquery_timeline.js
+++ ifcb/static/jquery_timeline.js
@@ -1,66 +1,30 @@
 (function($) {
     $.fn.extend({
-       timeline: function() {
+	timeline: function() {
             return this.each(function() {
                 var $this = $(this);
-		var timeline;
-		console.log('loading data series...');
-		$.getJSON('/api/volume', function(volume) {
-		    // Create and populate a data table.
-		    var data = [];
-		    $.each(volume, function(ix, day_volume) {
-			var bin_count = day_volume.bin_count;
-			var date = day_volume.day; // should be 'date'
-			var gb = day_volume.gb;
-			var year = date.split('-')[0];
-			var month = date.split('-')[1];
-			var day = date.split('-')[2];
-			var start = new Date(year, month-1, day)
-			var end = new Date(year, month-1, day);
-			end.setHours(end.getHours() + 24);
-
-			// create item with actual number
-			var height = Math.round(gb * 15);
-			var style = 'height:' + height + 'px;'
-			var color = '#ff0000';
-			style = 'height:' + height + 'px;' +
-			    'background-color: ' + color + ';'+
-			    'border: 1px solid ' + color + ';';
-			var actual = '<div class="bar" style="' + style + '" ' +
-			    ' title="'+gb+'GB"></div>';
-			var item = {
-			    'group': 'Data volume',
-			    'start': start,
-			    'end': end,
-			    'content': actual
-			};
-			data.push(item);
-		    });
-
-		    // specify options
-		    var options = {
-			"width":  "100%",
-			"height": "200px",
-			"style": "box"
-		    };
-
-		    // Instantiate our timeline object.
+		function screenToDate(event) {
+		    var x = event.clientX - $this.find('div.timeline-frame div').offset().left;
+		    return new Date(event.data.timeline.screenToTime(x))
+		}
+		$this.bind('showdata', {timeline:timeline}, function(event, data, options) {
+		    if(options == undefined) {
+			options = {
+			    "width":  "100%",
+			    "height": "200px",
+			    "style": "box"
+			}
+		    }
 		    console.log('trying to install timeline on '+$this.get(0));
 		    timeline = new links.Timeline($this.get(0))
 		    timeline.recalcConversion();
-		    function screenToDate(event) {
-			var x = event.clientX - $this.find('div.timeline-frame div').offset().left;
-			return new Date(event.data.timeline.screenToTime(x))
-		    }
+		    timeline.draw(data, options);
 		    $this.bind('mousemove', {timeline:timeline}, function(event) {
-			$this.trigger('dateHover', screenToDate(event));
+			$this.trigger('dateHover', [screenToDate(event), event.clientX]);
 		    });
 		    $this.bind('click', {timeline:timeline}, function(event) {
 			$this.trigger('dateClick', screenToDate(event));
 		    });
-
-		    // Draw our timeline with the created data and options
-		    timeline.draw(data, options);
 		});
             });
         }//timeline
