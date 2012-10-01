17f4be2a107a5fee0e0745e969c8d9c378ce4dda
joefutrelle <jfutrelle@whoi.edu>
2012-10-01 17:33:12 +0000

    fixing race condition having to do with atomicity of output file and ack.
    There is still a race condition which is completely impossible to avoid AFAIK but it will be much harder to trigger it now.
    When it is triggered, and then the job gets re-executed, the result will be two provenance records for the same job,
    one of which refers to the partially-completed job. The completed output file will be present in this case so can be recovered
    without re-doing processing.
    
    also put a filesystem space check and hardcoded it for now to start failing when there is <100MB free space on the disk,
    so as to prevent it from becoming 100% full.

--- habcam/image/imagestack.py
+++ habcam/image/imagestack.py
@@ -1,6 +1,6 @@
 from oii.procutil import Process
 import sys
-from oii.utils import Struct, md5_file, gen_id
+from oii.utils import Struct, md5_file, gen_id, freespace
 import os
 import re
 from oii.workflow.rabbit import Job, JobExit, WIN, FAIL, SKIP, PASS
@@ -26,30 +26,39 @@ class ImageStackWorker(ProcessWorker):
         return ''
     def get_parameters(self,message):
         img_in = message
+        if not os.path.exists(img_in):
+            self.log('FAIL input file does not exist: %s %s %s' % (self.config.out_dir, img_in, img_out))
+            raise JobExit('Input file does not exist: %s' % img_in, FAIL)
         img_out_file = re.sub(self.config.in_prefix,'',re.sub(r'\.tif','.png',img_in)).lstrip('/')
         print 'img_out_file = %s' % img_out_file
         img_out = os.path.join(self.config.out_dir,img_out_file)
         print 'img_out = %s' % img_out
         if os.path.exists(img_out):
-            self.log('SKIP already exists: %s %s %s' % (self.config.out_dir, img_in, img_out))
+            self.log('SKIP output file already exists: %s %s %s' % (self.config.out_dir, img_in, img_out))
             raise JobExit('Output file already exists: %s' % img_out, SKIP)
         od = os.path.dirname(img_out)
+        if freespace(od) < 104857600: # 100MB
+            msg = 'free disk space in output location <100MB: %s' % od
+            self.log('WARNING %s' % msg)
+            raise JobExit(msg, FAIL)
         if not os.path.exists(od):
             os.makedirs(od)
-        return dict(img_in=img_in,img_out=img_out,start_time=iso8601())
+        tmp_out = re.sub(r'\.png','_part.png',img_out)
+        return dict(img_in=img_in,img_out=tmp_out,final_out=img_out,start_time=iso8601())
     def win_callback(self,params):
         start_time = params['start_time']
         end_time = iso8601()
         img_in = params['img_in']
-        img_out = params['img_out']
+        tmp_out = params['img_out'] # temporary output file
+        img_out = params['final_out'] # final output file
         process_id = gen_id()
-        if not os.path.exists(img_out):
-            self.log('FAIL output file does not exist: %s' % img_out)
+        if not os.path.exists(tmp_out):
+            self.log('FAIL temporary output file does not exist: %s' % tmp_out)
             return FAIL
         in_md5 = md5_file(img_in)
-        out_md5 = md5_file(img_out)
+        out_md5 = md5_file(tmp_out)
         in_length = os.stat(img_in).st_size
-        out_length = os.stat(img_out).st_size
+        out_length = os.stat(tmp_out).st_size
         used = {
             'process_id': process_id,
             'algorithm_id': '201203_ic', # FIXME uncontrolled
@@ -72,8 +81,15 @@ class ImageStackWorker(ProcessWorker):
             }
         # FIXME emit provenance record
         prov_qname = '%s_prov' % self.qname
-        self.enqueue(json.dumps(used), prov_qname)
-        self.enqueue(json.dumps(generated_by), prov_qname)
+        try:
+            self.enqueue(json.dumps(used), prov_qname)
+            self.enqueue(json.dumps(generated_by), prov_qname)
+        except:
+            raise JobExit('Failed to enqueue provenance records', FAIL)
+        try:
+            os.rename(tmp_out, img_out)
+        except:
+            raise JobExit('Cannot move temporary file into place: %s -> %s' % (tmp_out, img_out), FAIL)
         return WIN
     def fail_callback(self,params):
         return FAIL
