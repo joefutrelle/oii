436cdea473efa8ef2f69ebfadd4bc33ef81b765e
joefutrelle <jfutrelle@whoi.edu>
2012-11-08 17:17:15 +0000

    OK, percent picking is now done with horizontal sliders.

--- webapi/static/percent_picker.js
+++ webapi/static/percent_picker.js
@@ -1,31 +1,30 @@
 // allow the user to choose a percent.
-// calls back every time the user moves the percent control, with the value
-// requires knob.js
-// this doesn't do much more than knob.js does, but handles range, basic styling, and focus
+// calls back every time the user changes the percent value
 (function($) {
     $.fn.extend({
         percentPicker: function(callback) {
 	    return this.each(function() {
 		var $this = $(this);
-		$this.append('<input class="percentKnob" type="text">')
-		    .find('.percentKnob')
-		    .attr('value',0)
-		    .attr('data-min',0)
-		    .attr('data-max',100)
-		    .attr('data-width',100)
-		    .attr('data-height',100)
-		    .attr('data-thickness',1)
-		    .attr('data-fgColor','#222222')
-		    .attr('data-bgColor','gray')
-		    .knob({'change': function(value) {
-			$this.find('.percentKnob').css('color','white');
-			if(callback != undefined) {
-			    callback(value);
+		function updateSliderValue() {
+		    var val = $this.find('.percentSlider').slider('value');
+		    $this.find('.percentValue').html(val+'%');
+		    return val;
+		}
+		$this.append('<div><span class="percentSlider"></span><span class="percentValue"></span></div>')
+		    .find('div').css('display','inline-block')
+		    .find('.percentSlider:last')
+		    .slider({
+			orientation: 'horizontal',
+			range: 'min',
+			max: 100,
+			value: 50,
+			slide: updateSliderValue,
+			change: function() {
+			    var val = updateSliderValue();
+			    callback(val);
 			}
-		    }}).find('.percentKnob').focus(function(event) {
-			// disallow focusing on text
-			$(this).blur();
 		    });
+		updateSliderValue();
 	    });
 	}
     });
