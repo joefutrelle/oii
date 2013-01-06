f3434b29cd3c7cfa38599e0ad8e6683689ae0093
joefutrelle <jfutrelle@whoi.edu>
2013-01-06 20:32:46 +0000

    bad bug in gw, fixed.

--- image/color.py
+++ image/color.py
@@ -4,11 +4,11 @@ from skimage.color import gray2rgb, rgb2hsv, hsv2rgb, rgb2gray
 def gray_world(a):
     """Gray-world color balance"""
     (_,_,c) = a.shape
-    means = [np.mean([a[:,:,n] for n in range(c)])]
+    means = [np.mean(a[:,:,n]) for n in range(c)]
     gray = np.mean(means)
     return a * (gray / means)
 
-def scale_saturation(a):
+def scale_saturation(a,saturation=1.0):
     hsv = rgb2hsv(a)
     hsv[:,:,1] = (hsv[:,:,1] * saturation).clip(0.,1.)
     return hsv2rgb(hsv)
