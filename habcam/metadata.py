0535465d443381885f9c69e2938aee07eb21bd59
joefutrelle <jfutrelle@whoi.edu>
2013-01-07 21:44:28 +0000

    now can fetch metadata for a whole ten-minute bin

--- habcam/metadata.py
+++ habcam/metadata.py
@@ -13,12 +13,25 @@ psql.extensions.register_type(DEC2FLOAT)
 class Metadata(object):
     def __init__(self,config):
         self.psql_connect = config.psql_connect
-    def json(self,imagename):
+    def image(self,imagename):
         imagename = re.sub(r'\.[a-z]+$','',imagename)
         with xa(self.psql_connect) as (c,_):
             db = c.cursor(cursor_factory=RealDictCursor)
             db.execute('select * from web_service_image_metadata where imagename like %s',(imagename+'%',))
-            return json.dumps(db.fetchone())
+            return db.fetchone()
+    def bin(self,bin_lid):
+        """bin lid is lid of 10-minute bin (e.g., 201203.20120623.1220)"""
+        pattern = re.sub(r'\d$','',bin_lid) + '%'
+        print pattern
+        with xa(self.psql_connect) as (c,_):
+            db = c.cursor(cursor_factory=RealDictCursor)
+            db.execute('select * from web_service_image_metadata where imagename like %s',(bin_lid+'%',))
+            return db.fetchall()
+    def json(self,imagename):
+        return json.dumps(self.image(imagename))
+    def json_bin(self,bin_lid):
+        """bin lid is lid of 10-minute bin (e.g., 201203.20120623.1220)"""
+        return json.dumps(self.bin(bin_lid))
 
 import sys
 
@@ -30,10 +43,13 @@ if __name__=='__main__':
     psql_connect = 'host=molamola.whoi.edu user=%s password=%s dbname=habcam_v3' % (u,p)
     config = Derp()
     config.psql_connect = psql_connect
+    metadata = Metadata(config)
     with xa(psql_connect) as (c,db):
         db.execute('select imagename from web_service_image_metadata order by random() limit 1')
         for row in db.fetchall():
             imagename = row[0]
     print 'imagename = %s' % imagename
-    metadata = Metadata(config)
-    print metadata.json(imagename)
+    bin_lid = re.sub(r'\d{5}\.\d+\.[a-zA-Z][a-zA-Z0-9]+$','',imagename)
+    print 'bin_lid = %s' % bin_lid
+    print metadata.json_bin(bin_lid)
+
