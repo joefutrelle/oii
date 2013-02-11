231f4e271abf6ef53d14129e563c8f3e995f6db0
joefutrelle <jfutrelle@whoi.edu>
2013-02-11 21:10:47 +0000

    moved all config information into an external config file.

--- ifcb/accession.py
+++ ifcb/accession.py
@@ -1,8 +1,10 @@
+import sys
 import logging
 import time
 import json
 import datetime
 
+from oii.config import get_config
 from oii.psql import xa
 from oii.iopipes import LocalFileSource
 from oii.ifcb.db import IfcbFeed, IfcbFixity
@@ -10,15 +12,17 @@ from oii.resolver import parse_stream
 from oii.ifcb.formats import integrity
 from oii.times import text2utcdatetime
 
-from celery import Celery
+#from celery import Celery
 
-celery = Celery('oii.ifcb.accession')
+#celery = Celery('oii.ifcb.accession')
 
-RESOLVER_FILE='oii/ifcb/mvco.xml'
-PSQL_CONNECT='user=testing password=********* dbname=ditylum'
+# example config file
+# resolver = oii/ifcb/mvco.xml
+# [ditylum]
+# psql_connect = user=foobar password=bazquux dbname=ditylum
 
-def list_adcs(time_series,after_year=2012):
-    r = parse_stream(RESOLVER_FILE) # FIXME hardcoded
+def list_adcs(time_series,resolver,after_year=2012):
+    r = parse_stream(resolver)
     for s in r['list_adcs'].resolve_all(time_series=time_series): # FIXME hardcoded
         date = time.strptime(s.date, s.date_format)
         if date.tm_year > after_year:
@@ -26,10 +30,10 @@ def list_adcs(time_series,after_year=2012):
         else:
             logging.info('%s SKIP, out of date range' % s.pid)
 
-def list_new_filesets(time_series,after_year=2012):
-    feed = IfcbFeed(PSQL_CONNECT) # FIXME hardcoded
-    r = parse_stream(RESOLVER_FILE) # FIXME hardcoded
-    for s in list_adcs(time_series,after_year):
+def list_new_filesets(time_series,psql_connect,resolver,after_year=2012):
+    feed = IfcbFeed(psql_connect)
+    r = parse_stream(resolver)
+    for s in list_adcs(time_series,resolver,after_year):
         if feed.exists(s.pid):
             logging.info('%s EXISTS in time series %s' % (s.pid, time_series))
         else:
@@ -49,17 +53,22 @@ def check_integrity(pid, hdr_path, adc_path, roi_path, schema_version):
     logging.info('%s PASS integrity check %s' % (pid, roi_path))
 
 if __name__=='__main__':
-    time_series='ditylum'
+    try:
+        time_series=sys.argv[2]
+        config = get_config(sys.argv[1], time_series)
+    except:
+        sys.stderr.write('usage: [python] oii/ifcb/accession.py [config file] [time series name]\n')
+        sys.exit(-1)
     logging.basicConfig(level=logging.INFO)
-    fx = IfcbFixity(PSQL_CONNECT) # FIXME
-    feed = IfcbFeed(PSQL_CONNECT) # FIXME hardcoded
-    with xa(PSQL_CONNECT) as (c, db):
-        for s in list_new_filesets(time_series,after_year=2011):
+    fx = IfcbFixity(config.psql_connect)
+    feed = IfcbFeed(config.psql_connect)
+    with xa(config.psql_connect) as (c, db):
+        for s in list_new_filesets(time_series,config.psql_connect,config.resolver,after_year=2011): # FIXME hardcoded
             try:
                 check_integrity(s.pid, s.hdr_path, s.adc_path, s.roi_path, s.schema_version)
             except Exception, e:
                 logging.warn('%s FAIL integrity checks: %s' % (s.pid, e))
-                break
+                continue
             # hot diggity, we've got some good data
             # compute fixity
             try:
@@ -72,7 +81,7 @@ if __name__=='__main__':
             except:
                 logging.error('%s FAIL fixity cannot be computed!' % s.pid)
                 c.rollback()
-                break
+                continue
             # register bin
             try:
                 ts = text2utcdatetime(s.date, s.date_format)
@@ -81,4 +90,5 @@ if __name__=='__main__':
                 logging.info('%s DONE' % s.pid)
             except:
                 logging.error('%s FAILED' % s.pid)
-                break
+                continue
+
