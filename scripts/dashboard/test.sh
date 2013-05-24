857baea89b9f07fe4b5e51f1437397268b4eda68
joefutrelle <jfutrelle@whoi.edu>
2013-05-24 17:37:53 +0000

    tests two time series

--- scripts/dashboard/test.sh
+++ scripts/dashboard/test.sh
@@ -1,5 +1,10 @@
 echo 'Downloading IFCB data ...'
 
+SYSTEM_USER=ifcb
+
+# MVCO data
+TIME_SERIES=mvco_test
+
 # these are just for testing
 DAY=IFCB5_2013_141
 BIN=${DAY}_113957
@@ -15,8 +20,37 @@ chown -R $SYSTEM_USER:$SYSTEM_USER /home/$SYSTEM_USER/data
 echo 'Initiating accession ...'
 sudo -u $SYSTEM_USER bash /home/$SYSTEM_USER/accession.sh $TIME_SERIES
 
-echo 'Waiting one minute ...'
-sleep 60
+echo 'Waiting 15s ...'
+sleep 15
+
+echo 'Tailing logs ...'
+tail /var/log/supervisor/*.log
+
+echo 'Querying database ...'
+sudo -u postgres psql $TIME_SERIES -P pager=off -c 'select * from bins'
+sudo -u postgres psql $TIME_SERIES -P pager=off -c 'select * from fixity'
+
+# Saltpond data
+TIME_SERIES=saltpond_test
+
+# these are just for testing
+YEAR=D2013
+DAY=D20130522
+BIN=D20130522T140308_IFCB012
+DATA_DIR=/home/$SYSTEM_USER/data2/$YEAR/$DAY
+
+mkdir -p $DATA_DIR
+for f in hdr adc roi; do
+    curl http://ifcb-data.whoi.edu/mvco/${BIN}.${f} > $DATA_DIR/${BIN}.${f}
+done
+
+chown -R $SYSTEM_USER:$SYSTEM_USER /home/$SYSTEM_USER/data2
+
+echo 'Initiating accession ...'
+sudo -u $SYSTEM_USER bash /home/$SYSTEM_USER/accession.sh $TIME_SERIES
+
+echo 'Waiting 15s ...'
+sleep 15
 
 echo 'Tailing logs ...'
 tail /var/log/supervisor/*.log
