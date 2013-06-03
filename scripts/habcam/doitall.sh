d10a384b0ad64b2a3bbd63fadd0fec36735f6dda
joefutrelle <jfutrelle@whoi.edu>
2013-06-03 20:36:55 +0000

    a bit of optimization

--- scripts/habcam/doitall.sh
+++ scripts/habcam/doitall.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-PYTHONPATH=/home/habcam/ic
+export PYTHONPATH=/home/habcam/ic
+cd /home/habcam/ic
 LEARN=$(head -1 learn_bin.conf)
 LOGS_DIR=/habcam/nmfs/proc/logs
 while read bin_lid; do
-    if [[ "$bin_lid" > "20130603_1800" ]]; then
+    if [[ "$bin_lid" > "20130603_1730" ]]; then
 	LOG_DIR=$LOGS_DIR/${bin_lid}
 	mkdir -p $LOG_DIR
 	LOGFILE=$LOG_DIR/${bin_lid}.$(date +%s).log
-	python oii/scripts/nosplit_lightfield_batch.py $bin_lid $LEARN > $LOGFILE
-	gzip $LOGFILE
+	/usr/bin/python ./oii/scripts/nosplit_lightfield_batch.py $bin_lid $LEARN > $LOGFILE
+	/bin/gzip $LOGFILE
     fi
 done < <\
-(find /habcam -type d -regextype posix-egrep -regex /habcam/nmfs/[0-9]{6}.*[0-9]{8}*_[0-9]{4}* \
+(/usr/bin/find /habcam -maxdepth 5 -type d -regextype posix-egrep -regex /habcam/nmfs/[0-9]{6}.*[0-9]{8}*_[0-9]{4}* \
 -exec basename {} \;)
