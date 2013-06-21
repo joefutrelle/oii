d46aae919391f693f6ba8b261c0ffb2fa81c1766
joefutrelle <jfutrelle@whoi.edu>
2013-06-21 19:11:43 +0000

    now has a date range slider. nothing is properly initialized and the epoch is hardcoded as jan 1 2006.

--- ifcb/static/bar.js
+++ ifcb/static/bar.js
@@ -22,21 +22,24 @@ function loadImages() {
 	if(elementInViewport(elt)) {
 	    img_src = $(elt).data('img_src');
 	    console.log(img_src+' in viewport');
-	    $(elt).find('a').append('<img src="'+img_src+'">');
+	    $(elt).find('a').append('<img src="'+img_src+'" alt="'+img_src+'">');
 	    $(elt).removeClass('roi_image_unloaded').addClass('roi_image');
+	    $(elt).css('width','auto');
+	    $(elt).css('height','auto');
 	}
     });
 }
 function showit() {
     var class_label = $('#class_select').val();
     var threshold = $('#threshold').slider('value') / 100.0;
-    //$.getJSON('/mvco/api/autoclass/rois_of_class/'+class_label+'/threshold/'+threshold+'/start/2012-07-04/end/2012-07-05', function(r) {
+    var startDate = $('#date_range').data('startDate').toISOString();
+    var endDate = $('#date_range').data('endDate').toISOString();
     $('#images').empty().append('please wait...');
-    $.getJSON('/mvco/api/autoclass/rois_of_class/'+class_label+'/threshold/'+threshold, function(r) {
+    $.getJSON('/mvco/api/autoclass/rois_of_class/'+class_label+'/threshold/'+threshold+'/start/'+startDate+'/end/'+endDate, function(r) {
 	$('#images').empty();
 	$.each(r, function(ix, roi_pid) {
-	    if(ix < 100) {
-		$('#images').append('<div><a href="'+roi_pid+'.html"></a>'+roi_pid+'</div>')
+	    if(ix < 1000) {
+		$('#images').append('<div style="display:inline-block;width:200px;height:200px"><a href="'+roi_pid+'.html"></a></div>')
 		    .find('div:last')
 		    .addClass('roi_image_unloaded')
 		    .data('img_src',roi_pid+'.png');
@@ -45,19 +48,38 @@ function showit() {
 	loadImages();
     });
 }
+var EPOCH = (new Date(2006,0,1)).getTime();
+var NOW = (new Date()).getTime();
 $(document).ready(function() {
-    $('#main').append('<select id="class_select"></select>').find('#class_select');
+    $('#main').append('<select id="class_select"></select>');
     $('#main').append('<div style="display: inline-block; width: 300px" id="threshold">').find('#threshold')
 	.slider({
 	    min: 1,
 	    max: 99,
 	    value: 99,
 	    change: function () {
-		$('#threshval').empty().append('' + $('#threshold').slider('value') / 100.0);
+		$('#thresh_val').empty().append('' + $('#threshold').slider('value') / 100.0);
 		showit();
 	    }
 	});
-    $('#main').append('<span id="threshval"></span>');
+    $('#main').append('<span id="thresh_val"></span>');
+    $('#main').append('<div style="display: inline-block; width: 300px" id="date_range">').find('#date_range')
+	.slider({
+	    range: true,
+	    min: 1,
+	    max: 1000,
+	    values: [800, 900],
+	    change: function() {
+		var values = $('#date_range').slider('values');
+		var startDate = new Date(EPOCH + ((values[0] / 1000) * (NOW - EPOCH)));
+		var endDate = new Date(EPOCH + ((values[1] / 1000) * (NOW - EPOCH)));
+		$('#date_range').data('startDate',startDate);
+		$('#date_range').data('endDate',endDate);
+		$('#date_range_val').empty().append(startDate.toISOString() + ' - ' + endDate.toISOString());
+		showit();
+	    }
+	});
+    $('#main').append('<span id="date_range_val"></span>');
     $('#main').append('<div id="images"></div>').find('#images');
     $.getJSON('/mvco/api/autoclass/list_classes', function(r) {
 	$.each(r, function(ix, class_label) {
