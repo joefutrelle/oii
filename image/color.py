725e3cf796560bdd33703efc3a590837a35aa356
joefutrelle <jfutrelle@whoi.edu>
2013-07-30 20:36:55 +0000

    gray_world now accepts gray param, new function for computing gray param

--- image/color.py
+++ image/color.py
@@ -1,11 +1,18 @@
 import numpy as np
 from skimage.color import gray2rgb, rgb2hsv, hsv2rgb, rgb2gray
 
-def gray_world(a):
+def gray_value(a):
+    # computes grayworld gray value
+    (_,_,c) = a.shape
+    means = [np.mean(a[:,:,n]) for n in range(c)]
+    return np.mean(means)
+
+def gray_world(a,gray=None):
     """Gray-world color balance"""
     (_,_,c) = a.shape
     means = [np.mean(a[:,:,n]) for n in range(c)]
-    gray = np.mean(means)
+    if gray is None:
+        gray = np.mean(means)
     return a * (gray / means)
 
 def scale_saturation(a,saturation=1.0):
