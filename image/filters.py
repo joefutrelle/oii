b06f27190e6fab2c1413d475cc2c1941fdf3cc2a
joefutrelle <jfutrelle@whoi.edu>
2013-09-15 18:57:54 +0000

    local variance

--- image/filters.py
+++ image/filters.py
@@ -1,8 +1,9 @@
 import numpy as np
 
-from scipy.ndimage.filters import gaussian_filter
+from scipy.ndimage.filters import generic_filter, gaussian_filter
 
 from skimage.exposure import rescale_intensity
+from skimage.morphology import square
 
 def unsharp_mask(image,size,a=3):
     um = gaussian_filter(image,size)
@@ -21,3 +22,6 @@ def contrast_stretch(image,amount=2):
     low = np.percentile(image,amount)
     high = np.percentile(image,100-amount)
     return rescale_intensity(image,in_range=(low,high))
+
+def local_variance(image,footprint=square(3),mode='reflect',cval=0.0):
+    return generic_filter(image, np.var, footprint=footprint, mode=mode,cval=cval)
