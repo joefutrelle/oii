68e5db738bacc29fa769095ffdf4b49d13b5f150
joefutrelle <jfutrelle@whoi.edu>
2013-09-18 18:50:39 +0000

    these cron utilities are currently essential and it was a mistake not to check them in!

---
+++ ifcb/workflow/blob_cron.py
@@ -0,0 +1,37 @@
+import sys
+import logging
+import time
+import json
+import datetime
+
+from oii.resolver import parse_stream
+from oii.ifcb.db import IfcbFeed
+from oii.utils import gen_id
+from oii.config import get_config
+
+from oii.ifcb.workflow.blob_extraction import extract_blobs
+
+# example config file
+# resolver = oii/ifcb/mvco.xml
+# [ditylum]
+# psql_connect = user=foobar password=bazquux dbname=ditylum
+
+CONFIG_FILE = './blob.conf'
+
+def enqueue_blobs(time_series,queue):
+    """config needs psql_connect, resolver"""
+    config = get_config(CONFIG_FILE, time_series)
+    feed = IfcbFeed(config.psql_connect)
+    r = parse_stream(config.resolver)
+    blob_resolver = r['mvco_blob']
+    pid_resolver = r['pid']
+    for lid in feed.latest_bins(n=1000):
+        if blob_resolver.resolve(pid=lid,time_series=time_series) is None:
+            pid = pid_resolver.resolve(pid=lid,time_series=time_series).bin_pid
+            print 'No blobs found for %s, enqueuing' % pid
+            extract_blobs.apply_async(args=[time_series, pid],queue=queue)
+
+if __name__=='__main__':
+    time_series = sys.argv[1]
+    queue = sys.argv[2]
+    enqueue_blobs(time_series,queue)
