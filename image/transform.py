219f1ffe942da15aa84251c79d8eb975028f7e51
joefutrelle <jfutrelle@whoi.edu>
2013-09-23 20:14:09 +0000

    resizing without off by one errors

---
+++ image/transform.py
@@ -0,0 +1,12 @@
+import scipy.ndimage as ndi
+
+def resize(img,shape,order=0):
+    """Resize using ndi zoom. Corrects for:
+    http://scipy-user.10969.n7.nabble.com/wrong-output-shape-calculation-in-scipy-ndimage-interpolation-zoom-td17289.html"""
+    if len(img.shape)==3:
+        return np.dstack([resize(img[:,:,c],shape,order) for c in range(img.shape[2])])
+    else:
+        sy = (1. * shape[0] / img.shape[0]) + 0.0000000001
+        sx = (1. * shape[1] / img.shape[1]) + 0.0000000001
+        return ndi.interpolation.zoom(img,(sy,sx),order=order)
+        return ndi.interpolation.zoom(img,1. * shape / img.shape,order=order)
