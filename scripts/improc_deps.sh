917abd497b73fe3016aaf274091aad435df17d80
joefutrelle <jfutrelle@whoi.edu>
2013-10-07 19:27:42 +0000

    fftw3 support

--- scripts/improc_deps.sh
+++ scripts/improc_deps.sh
@@ -12,12 +12,14 @@ apt-get update
 
 # install required aptitude packages
 
-apt-get install -y libxml2-dev libxslt-dev zlib1g-dev cython python-pip python-lxml python-imaging python-pika python-flask liblapack-dev gfortran libblas-dev g++ python-dev python-pip libfreeimage3 python-numpy python-scipy python-matplotlib
+apt-get install -y libxml2-dev libxslt-dev zlib1g-dev cython python-pip python-lxml python-imaging python-pika python-flask liblapack-dev gfortran libblas-dev g++ python-dev python-pip libfreeimage3 python-numpy python-scipy python-matplotlib fftw3-dev
 
 # now use pip for remaining python packages
 
 pip install pytz
 pip install scikits-image
 pip install scikits.learn
+pip install pyfftw
+pip install anfft
 pip install phasepack==1.1
 
