d8cfa2580d35b7e50b23c90a22d46e8b6f8afc59
mostlymark <marknye@clubofhumanbeings.com>
2014-03-30 19:07:08 +0000

    add configuration database mappings

--- ifcb/adminapi.py
+++ ifcb/adminapi.py
@@ -1,15 +1,57 @@
 from flask import Flask, jsonify, abort
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 # IFCB restful configuration API
 # written by Mark Nye (marknye@clubofhumanbeings.com), March 2014
 
+# config (refactor later)
+BASEPATH = '/admin/api/v1.0'
+DBENGINE = 'sqlite:///:memory:'
+
+# configuration database mapping
+# this will also need to be refactored, but keep in one file for now
+Base = declarative_base()
+
+class TimeSeries(Base):
+
+    __tablename__ = 'timeseries'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    enabled = Column(Boolean)
+
+    def __repr__(self):
+        return "<TimeSeries(name='%s')" % self.name
+
+class SystemPath(Base):
+
+    __tablename__ = 'systempaths'
+    id = Column(Integer, primary_key=True)
+    timeseries_id = Column(Integer, ForeignKey('timeseries.id'))
+    path = Column(String)
+
+    timeseries = relationship("TimeSeries",
+        backref=backref('systempaths', order_by=id))
+
+    def __repr__(self):
+        return "<SystemPath(path='%s')" % self.path
+
+
 def validate_path(path):
     pass
 
+# create flask app
 app = Flask(__name__)
 app.debug = True
 
-BASEPATH = '/admin/api/v1.0'
+# create db engine and database session
+dbengine = create_engine(DBENGINE, echo=True)
+Session = sessionmaker(bind=dbengine)
+
 
 @app.route(BASEPATH + '/timeseries', methods = ['GET'])
 # return all timeseries configurations
@@ -38,4 +80,5 @@ def delete_timeseries():
 
 
 if __name__=='__main__':
+    Base.metadata.create_all(dbengine)
     app.run(host='0.0.0.0',port=8080)
