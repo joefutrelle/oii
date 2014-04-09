2d1950b28759ac5fa6d568978c08be2c50d69da8
mostlymark <marknye@clubofhumanbeings.com>
2014-04-09 18:11:22 +0000

    add timeseries group elements

--- ifcb/admin/restless.py
+++ ifcb/admin/restless.py
@@ -20,18 +20,22 @@ Session = sessionmaker(bind=dbengine)
 session = Session()
 
 manager = flask.ext.restless.APIManager(app, session=session)
-manager.create_api(TimeSeries, methods=['GET', 'PUT', 'POST', 'DELETE'])
-manager.create_api(SystemPath, methods=['GET', 'POST', 'DELETE'])
+manager.create_api(TimeSeries, url_prefix='/admin/api/v1', methods=['GET', 'PUT', 'POST', 'DELETE'])
+manager.create_api(SystemPath, url_prefix='/admin/api/v1', methods=['GET', 'POST', 'DELETE'])
 
 
 if __name__=='__main__':
     Base.metadata.create_all(dbengine)
-    ts = TimeSeries(name = 'testseries1',enabled = False)
+    ts = TimeSeries(name = 'Pond Water',enabled = False)
     path = SystemPath(path = '/Users/marknye')
+    path2 = SystemPath(path = '/Users/marknye/Documents')
     ts.systempaths.append(path)
+    ts.systempaths.append(path2)
     session.add(ts)
-    session.commit()
-    session.add(ts)
+    ts2 = TimeSeries(name = 'Ocean Water',enabled = False)
+    path3 = SystemPath(path = '/Users/marknye/Desktop')
+    ts2.systempaths.append(path3)
+    session.add(ts2)
     session.commit()
     print app.url_map
     app.run(host='0.0.0.0',port=8080)
--- ifcb/admin/static/js/ifcbadmin.js
+++ ifcb/admin/static/js/ifcbadmin.js
@@ -1,61 +1,80 @@
 
 
 // create AngularJS application
-var ifcbAdmin = angular.module('ifcbAdmin', ['ngRoute']);
+var ifcbAdmin = angular.module('ifcbAdmin', ['ngRoute','restangular']);
 
-// application controllers container
-var controllers = {};
+// configure Restangular for flask restless endpoints
+ifcbAdmin.config(function(RestangularProvider) {
+
+    RestangularProvider.setBaseUrl('/admin/api/v1');
+
+    RestangularProvider.setResponseExtractor(function(response, operation) {
+        // This is a get for a list
+        var newResponse;
+        if (operation === 'getList') {
+            // Return the result objects as an array and attach the metadata
+            newResponse = response.objects;
+            newResponse.metadata = {
+                numResults: response.num_results,
+                page: response.page,
+                totalPages: response.total_pages
+            };
+        } else {
+            // This is an element
+            newResponse = response;
+        }
+      return newResponse;
+    });
+});
 
 // nav controller
 ifcbAdmin.controller('NavigationCtrl', ['$scope', '$location', function ($scope, $location) {
     $scope.isCurrentPath = function (path) {
       return $location.path() == path;
     };
-  }]);
+}]);
 
-// time series controller
-controllers.TimeSeriesController = function($scope) {
+ifcbAdmin.controller('TimeSeriesCtrl', ['$scope', 'Restangular', function ($scope, Restangular) {
 
-    $scope.timeseries = [
-        {
-            "enabled": false,
-            "name": "testseries1",
-            "systempaths": [
-                "/Users/marknye"
-            ],
-            "uri": "http://localhost:8080/admin/api/v1.0/timeseries/1"
-        }
-    ];
-};
+    var baseTimeSeries = Restangular.all('timeseries');
+    $scope.timeseries = baseTimeSeries.getList().$object;
+
+    $scope.editTimeSeries = function(ts) {
+        ts.edit = true;
+    }
+
+    $scope.saveTimeSeries = function(ts) {
+        delete ts.edit;
+    }
+
+}]);
 
 // users controller
-controllers.UsersController = function($scope) {
+ifcbAdmin.controller('UsersCtrl', ['$scope', function ($scope) {
 
     $scope.users = [];
-}
+}]);
 
 // my account controller
-controllers.MyAccountController = function($scope) {
+ifcbAdmin.controller('AccountCtrl', ['$scope', function ($scope) {
 
     $scope.myaccount = [];
-}
+}]);
 
-// bind controllers to application
-ifcbAdmin.controller(controllers);
 
 // define application routes
 ifcbAdmin.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
         when('/timeseries', {
-            controller: 'TimeSeriesController',
+            controller: 'TimeSeriesCtrl',
             templateUrl: 'views/TimeSeries.html'
             }).
         when('/users', {
-            controller: 'UsersController',
+            controller: 'UsersCtrl',
             templateUrl: 'views/Users.html'
             }).
         when('/myaccount', {
-            controller: 'MyAccountController',
+            controller: 'AccountCtrl',
             templateUrl: 'views/MyAccount.html'
             }).
         otherwise({
