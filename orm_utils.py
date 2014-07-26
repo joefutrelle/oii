e34e39d14b7e7f629cb0ed0428cd5f27c83ad01e
joefutrelle <jfutrelle@whoi.edu>
2014-07-26 15:47:47 +0000

    wrapping up #2966

--- orm_utils.py
+++ orm_utils.py
@@ -6,12 +6,16 @@ from oii.times import dt2utcdt
 def utc_datetime_attribute_instrument_listener(cls, key, inst):
     # here I'm assuming "key" is the class's attribute name - unclear from SQLAlchemy docs
     if isinstance(inst.property.columns[0].type, DateTime):
+        # listen for userland set operation and set timezone to UTC
         def setter(target, value, oldvalue, _):
             return dt2utcdt(value)
         event.listen(inst.property, 'set', setter, retval=True)
-        def loader(target, _):
+        # trigger a userland set operation
+        def upset(target, *_):
             setattr(target, key, getattr(target, key))
-        event.listen(cls, 'load', loader)
+        # do this on relevant instance events
+        for event_type in ['init', 'load', 'refresh']:
+            event.listen(cls, event_type, upset)
 
 def fix_utc(cls):
     event.listen(cls, 'attribute_instrument', utc_datetime_attribute_instrument_listener)
