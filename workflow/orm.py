356b780c48bec569343cdac28e9a7c4b5e0591ac
joefutrelle <jfutrelle@whoi.edu>
2014-07-29 21:23:51 +0000

    building out query functions, ... not sure if I want to wrap them. atomic access to next product that can be produced is working, as (ossibly inefficent) deletion of products that are no longer needed because everything that needs to be produced from them is available.

--- workflow/orm.py
+++ workflow/orm.py
@@ -31,8 +31,8 @@ product = Table('products', metadata,
                 Column('ts', DateTime(timezone=True)))
 
 product_deps = Table('product_deps', metadata,
-             Column('dependent', String, ForeignKey('products.pid')),
-             Column('depends_on', String, ForeignKey('products.pid')))
+                     Column('dependent', String, ForeignKey('products.pid')),
+                     Column('depends_on', String, ForeignKey('products.pid')))
 
 fix_utc(Product)
 mapper(Product, product, properties={
@@ -42,3 +42,31 @@ mapper(Product, product, properties={
                                    secondaryjoin=product.c.pid==product_deps.c.depends_on,
                                    backref='dependents')
 })
+
+# queries that range across entire product dependency hierarchies
+
+def get_next_product(session, dep_state='available', old_state='waiting', new_state='running'):
+    """find any product that is in state old_state and all of whose
+    dependents are in dep_state, and atomically set its state to new_state"""
+    p = session.query(Product).\
+        filter(Product.state==old_state).\
+        filter(Product.depends_on.any(Product.state==dep_state)).\
+        filter(~Product.depends_on.any(Product.state!=dep_state)).\
+        with_lockmode('update').\
+        first()
+    if p is None:
+        return None
+    p.changed('pop',new_state)
+    session.commit()
+    return p
+
+def delete_unneeded(session, dep_state='available'):
+    """delete all products that have dependents all of which are in dep_state"""
+    for product in session.query(Product).\
+        filter(Product.dependents.any()).\
+        filter(~Product.dependents.any(Product.state!='available')).\
+        with_lockmode('update'):
+        product.depends_on = []
+        product.dependents = []
+        session.delete(product)
+    session.commit()
--- workflow/product.py
+++ workflow/product.py
@@ -10,11 +10,12 @@ class Product(object):
         self.event = event
         self.ts = ts
         self.depends_on = []
-        if self.state is None:
-            self.changed('create')
+        self.changed(state=state,event=event,ts=ts)
     def __repr__(self):
         return '<Product %s (%s)>' % (self.pid, self.state)
-    def changed(self, event='create', state='new', ts=None):
+    def changed(self, event=None, state=None, ts=None):
+        if event is None: event = 'create'
+        if state is None: state = 'new'
         if ts is None:
             ts = secs2utcdatetime() # now
         self.state = state
