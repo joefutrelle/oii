e741f2180b86210fbef4612b38ef48dc1f414a20
joefutrelle <jfutrelle@whoi.edu>
2014-08-06 15:23:45 +0000

    example LDR2 script for generating workflow templates from a pid, then using the workflow ORM to track production. based on the "canny" toy workflow.

---
+++ scripts/wf.py
@@ -0,0 +1,75 @@
+import time
+import sys
+
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, func, distinct
+from sqlalchemy.orm import sessionmaker
+
+from oii.ldr import Resolver
+
+from oii.workflow.product_orm import Base, Product, Dependency, Products
+
+engine = sqlalchemy.create_engine('sqlite:///')
+Base.metadata.create_all(engine)
+session = sessionmaker(bind=engine)()
+
+resolver_xml = """
+<namespace name="wf">
+  <rule name="pid" distinct="pid lid product extension">
+    <var name="reid">[a-zA-Z][a-zA-Z0-9]*</var>
+    <match var="pid" pattern="([^_.]+)(_(${reid}))?(.(${reid}))?"
+	   groups="lid - product - extension"/>
+  </rule>
+  <rule name="deps" distinct="pid product upstream_product role">
+    <invoke rule="wf.pid" using="pid" toget="pid lid"/>
+    <vars names="product upstream_product role" delim=",">
+      <vals>${lid}_gray,${pid},color</vals>
+      <vals>${lid}_canny,${lid}_gray,gray</vals>
+      <vals>${lid}_final,${lid}_canny,overlay</vals>
+      <vals>${lid}_final,${pid},background</vals>
+    </vars>
+  </rule>
+  <rule name="products" distinct="product">
+    <invoke rule="wf.deps" using="pid" toget="product"/>
+  </rule>
+</namespace>
+"""
+R = Resolver(resolver_xml)
+
+pid='foo_raw.jpg'
+
+P = {}
+P[pid] = Product(pid=pid,state='available')
+p_lids = R.resolve('wf.products',pid=pid)
+for p_lid in [s['product'] for s in p_lids]:
+    P[p_lid] = Product(pid=p_lid, state='waiting')
+
+session.add_all(P.values())
+
+deps = R.resolve('wf.deps',pid=pid)
+for dep in deps:
+    Products.add_dep(session, P[dep['product']], P[dep['upstream_product']], dep['role'])
+
+session.commit()
+
+def do_work():
+    for worker_roles in [['color'], ['gray'], ['overlay','background']]:
+        p = Products.get_next(session, worker_roles)
+        if p is not None:
+            p.changed('start', 'running')
+            session.commit()
+            for wr in worker_roles:
+                print 'using %s for %s' % (p.deps_for_role(wr), wr)
+            time.sleep(1)
+            p.changed('complete', 'available')
+            session.commit()
+            print 'Completed %s' % p
+            return True
+    return False
+
+print 'starting work'
+
+while do_work():
+    print 'looking for more work'
+
+print 'done working'
