d404ba929b4cf37d7e6d8b50daaeef7be63b84b0
joefutrelle <jfutrelle@whoi.edu>
2014-08-19 18:45:38 +0000

    added identifiers module wired it into CSV representation to produce target pids added namespace support to pid resolver

---
+++ ifcb2/identifiers.py
@@ -0,0 +1,28 @@
+from oii.ifcb2 import get_rules
+from oii.ifcb2.formats.adc import TARGET_NUMBER
+
+BIN_KEY='binID'
+TARGET_KEY='pid'
+
+class IdentifierSyntaxError(Exception):
+    pass
+
+def parse_pid(pid):
+    """use the IFCB resolver to parse a pid"""
+    try:
+        return next(get_rules().ifcb.pid(pid))
+    except:
+        raise IdentifierSyntaxError(pid)
+
+def target_pid(bin_pid,target_number=1):
+    """bin_pid must have no product or extension,
+    but may have a namespace prefix"""
+    return '%s_%05d' % (bin_pid, target_number)
+
+def add_pids(targets,bin_pid,bin_key='binID',target_key='pid'):
+    """bin_pid must have no product or extension,
+    but may have a namespace prefix"""
+    for target in targets:
+        target[bin_key] = bin_pid
+        target[target_key] = target_pid(bin_pid,target[TARGET_NUMBER])
+        yield target
--- ifcb2/represent.py
+++ ifcb2/represent.py
@@ -1,22 +1,12 @@
 from oii.csvio import csv_str, csv_quote
 from oii.ifcb2.formats.adc import TARGET_NUMBER
 
-PID='pid'
-
-def add_bin_pid(targets, bin_pid):
-    for target in targets:
-        # add a binID and pid what are the right keys for these?
-        target['binID'] = '%s' % bin_pid
-        target['pid'] = '%s_%05d' % (bin_pid, target[TARGET_NUMBER])
-    return targets
-
-def targets2csv(targets,schema_cols):
-    """Given targets, produce a CSV representation in the specified schema"""
+def targets2csv(targets,schema_cols,headers=True):
+    """Given targets, produce a CSV representation in the specified schema;
+    targets should have had binID and pid added to them (see oii.ifcb2.identifiers)"""
     ks = schema_cols + ['binID','pid','stitched','targetNumber']
-    yield ','.join(ks)
+    if headers:
+        yield ','.join(ks)
     for target in targets:
-        # FIXME fake it till you make it
-        target['binID'] = 'placeholder'
-        target['pid'] = 'placeholder'
         # fetch all the data for this row as strings, emit
         yield ','.join(csv_quote(csv_str(target[k])) for k in ks)
