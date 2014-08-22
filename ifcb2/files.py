93bab6ce5318f73b0205297138b927955cb4a40b
joefutrelle <jfutrelle@whoi.edu>
2014-08-22 20:39:22 +0000

    broke out the pid-parsing function into two, one that takes an already-parsed pid.

--- ifcb2/files.py
+++ ifcb2/files.py
@@ -3,13 +3,18 @@ from oii.ifcb2.identifiers import parse_pid
 
 from oii.ldr import pprint
 
-def pid2file(pid):
+def pid2fileset(pid,roots):
     parsed_pid = parse_pid(pid)
-    for r in get_resolver().ifcb.local.data_roots():
-        root = r['root']
-        for f in get_resolver().ifcb.files.find_raw_fileset(root=root,**parsed_pid):
-            print f
+    return parsed_pid2fileset(parsed_pid,roots)
+
+def parsed_pid2fileset(parsed_pid,roots):
+    paths = {}
+    for root in roots:
+        for p in get_resolver().ifcb.files.find_raw_fileset(root=root,**parsed_pid):
+            paths.update(p)
+        if paths:
+            return paths
 
 import sys
 if __name__=='__main__':
-    pid2file(sys.argv[1])
+    print pid2fileset(sys.argv[1],roots=['/mnt/data/okeanos'])
