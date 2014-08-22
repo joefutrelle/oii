1716736f82ff31cf249e5d1e8c57dff8574a9425
joefutrelle <jfutrelle@whoi.edu>
2014-08-22 15:50:19 +0000

    raw stitching works.

--- ifcb2/stitching.py
+++ ifcb2/stitching.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from oii.ifcb2.formats.adc import TRIGGER, LEFT, BOTTOM, WIDTH, HEIGHT
 
 STITCHED='stitched'
@@ -31,7 +33,7 @@ def list_stitched_targets(targets):
     """Adjust a list of targets for stitching"""
     # in the stitching case we need to compute "stitched" flags based on pairs
     # correct image metrics
-    targets = list(targets) # consume any iterator
+    targets = [t.copy() for t in targets] # consume iterator non-destructively
     Bs = []
     for a,b in find_pairs(targets):
         (a[LEFT], a[BOTTOM], a[WIDTH], a[HEIGHT]) = stitched_box([a,b])
@@ -44,3 +46,20 @@ def list_stitched_targets(targets):
         if not STITCHED in target:
             target[STITCHED] = 0
     return targets
+
+# stitch with no noise fill
+def stitch_raw(targets,images,box=None,background=0):
+    # compute bounds relative to the camera field
+    if box is None:
+        box = stitched_box(targets)
+    (x,y,w,h) = box
+    # now we swap width and height to rotate the image 90 degrees
+    s = np.ones((w,h),dtype='uint8') * background
+    for (roi,image) in zip(targets,images):
+        rx = roi[LEFT] - x
+        ry = roi[BOTTOM] - y
+        rw = rx + roi[WIDTH]
+        rh = ry + roi[HEIGHT]
+        print s[rx:rw,ry:rh].shape, image.shape
+        s[rx:rw,ry:rh] = image
+    return s
