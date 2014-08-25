33e43e8b147630d2033261a04600defbab3c4c3c
joefutrelle <jfutrelle@whoi.edu>
2014-08-25 21:07:24 +0000

    new accession demo using new resolver code

--- ifcb2/ifcb_orm.py
+++ ifcb2/ifcb_orm.py
@@ -12,15 +12,10 @@ from sqlalchemy.orm import sessionmaker, relationship, backref
 from sqlalchemy import and_, or_, desc
 from sqlalchemy.sql.expression import func
 
-from oii.times import text2utcdatetime
-from oii.resolver import parse_stream
-from oii.times import text2utcdatetime
-
+from oii.times import text2utcdatetime, datetime2utcdatetime
+from oii.ifcb2 import get_resolver
+from oii.ifcb2.identifiers import parse_pid
 from oii.ifcb2.orm import Base, TimeSeries, DataDirectory, Bin, File, User
-
-def datetime2utcdatetime(dt):
-    # convert dt without timezone to one in utc
-    return datetime.fromtimestamp(calendar.timegm(dt.timetuple()), pytz.utc)
 
 def time_range(session, start_time, end_time):
     return session.query(Bin).\
@@ -70,51 +65,34 @@ def nearest_demo(session):
     cand = list(n_before) + list(n_after)
     print sorted(cand, key=lambda b: somedate - datetime2utcdatetime(b.sample_time))[:n]
 
-def query_demo(engine):
-    Session = sessionmaker()
-    Session.configure(bind=engine)
-    session = Session()
+def query_demo(session):
     time_range_demo(session)
     files_demo(session)
     data_volume_demo(session)
     nearest_demo(session)
-    session.close()
 
 # accession
-def list_adcs(time_series,resolver):
-    r = parse_stream(resolver)
-    for s in r['list_adcs'].resolve_all(time_series=time_series):
-        yield s
-
-def list_filesets(time_series,resolver):
-    r = parse_stream(resolver)
-    for s in list_adcs(time_series,resolver):
-        fs = r['fileset'].resolve(pid=s.pid,product='raw',time_series=time_series,day_dir=s.day_dir)
-        if fs is not None:
-            yield fs
-
-def accession_demo(engine):
+def accession_demo(session,root):
     # now accede
-    RESOLVER='resolver.xml'
-    TIME_SERIES='okeanos'
-    Session = sessionmaker()
-    Session.configure(bind=engine)
-    session = Session()
-    for fs in list_filesets(TIME_SERIES,RESOLVER):
-        print fs
-        ts = text2utcdatetime(fs.date, fs.date_format)
-        b = Bin(fs.lid, ts)
+    for fs in get_resolver().ifcb.files.list_raw_filesets(root):
+        lid = fs['lid']
+        try:
+            parsed = parse_pid(lid)
+        except:
+            print 'barf %s' % lid
+            raise
+        ts = text2utcdatetime(parsed['timestamp'], parsed['timestamp_format'])
+        b = Bin(lid, ts)
         session.add(b)
         # now make mostly bogus fixity entries
         now = datetime.now()
-        paths = [fs.hdr_path, fs.adc_path, fs.roi_path]
+        paths = [fs['hdr_path'], fs['adc_path'], fs['roi_path']]
         filetypes = ['hdr','adc','roi']
         for path,filetype in zip(paths,filetypes):
             length = os.stat(path).st_size
-            file = File(fs.lid, length, os.path.basename(path), filetype, 'abc123', now, path)
+            file = File(lid, length, os.path.basename(path), filetype, 'abc123', now, path)
             session.add(file)
     session.commit()
-    session.close()
 
 def get_sqlite_engine(delete=True):
     # first, toast db
@@ -126,21 +104,14 @@ def get_sqlite_engine(delete=True):
             pass
     return sqla.create_engine('sqlite:///%s' % DB_FILE)
 
-def psql_demo():
-    wipe = False
-    #engine = get_sqlite_engine(delete=wipe)
-    engine = sqla.create_engine('postgresql://ifcb:ifcb@localhost/testdb')
-    Base.metadata.create_all(engine)
-    if wipe: # deleted everything, so need to start
-        accession_demo(engine)
-    query_demo(engine)
+def get_psql_engine():
+    return sqla.create_engine('postgresql://ifcb:ifcb@localhost/testdb')
 
-if __name__=='__main__':
-    engine = sqla.create_engine('sqlite://')
-    Base.metadata.create_all(engine)
-    Session = sessionmaker()
-    Session.configure(bind=engine)
-    session = Session()
+def do_accession_demo(session,root):
+    accession_demo(session,root)
+    query_demo(session)
+
+def timeseries_demo(session):
     ts = TimeSeries(name='ts_one',description='First time series')
     ts.data_dirs.append(DataDirectory(path='/tmp/foo'))
     ts.data_dirs.append(DataDirectory(path='/blah/fnord'))
@@ -148,11 +119,17 @@ if __name__=='__main__':
     session.add(ts)
     session.add(user)
     session.commit()
-    session.close()
-    print 'closed session'
-    session = Session()
     for ts in session.query(TimeSeries):
         print ts
         print ts.data_dirs
     for u in session.query(User):
         print u
+
+if __name__=='__main__':
+    engine = get_sqlite_engine()
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+    timeseries_demo(session)
+    do_accession_demo(session,'/mnt/data/okeanos')
