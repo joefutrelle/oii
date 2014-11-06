4780df991decb3020d0c5acee6946e6328b12640
joefutrelle <jfutrelle@whoi.edu>
2014-11-06 18:47:44 +0000

    added borders and column ordering to asciitable

--- utils.py
+++ utils.py
@@ -410,14 +410,22 @@ def freespace(pth):
 def rpad(s,l,pad_string=' '):
     return s + (pad_string * (l - len(s)))
 
-def asciitable(dicts):
+def asciitable(dicts,disp_cols=None,none_msg=None,border=True):
     """produce an ASCII formatted columnar table from the dicts"""
-    # set of all keys in dicts
-    cols = sorted(list(set(reduce(lambda x,y: x+y, [d.keys() for d in dicts]))))
+    dicts = list(dicts)
+    if not dicts:
+        if none_msg is not None:
+            yield none_msg
+        return
+    if disp_cols is not None:
+        cols = disp_cols
+    else:
+        # set of all keys in dicts
+        cols = sorted(list(set(reduce(lambda x,y: x+y, [d.keys() for d in dicts]))))
     # compute col widths. initially wide enough for the column label
     widths = dict([(col,len(col)) for col in cols])
     # now create rows, and in doing so compute max width of each column
-    for row in dicts:
+    for row in list(dicts):
         for col in cols:
             try:
                 width = len(str(row[col]))
@@ -425,11 +433,21 @@ def asciitable(dicts):
                 width = 0
             if width > widths[col]:
                 widths[col] = width
+    def bord(line,border_char='|'):
+        if border:
+            return border_char + line + border_char
+        else:
+            return line
     # now print rows
-    yield ' | '.join([rpad(col,widths[col]) for col in cols])
-    yield '-+-'.join(['-' * widths[col] for col in cols])
+    spacer = bord('-+-'.join(['-' * widths[col] for col in cols]),'+')
+    if border:
+        yield spacer
+    yield bord(' | '.join([rpad(col,widths[col]) for col in cols]),'|')
+    yield spacer
     for row in dicts:
-        yield ' | '.join([rpad(str(row[col]),widths[col]) for col in cols])
+        yield bord(' | '.join([rpad(str(row[col]),widths[col]) for col in cols]),'|')
+    if border:
+        yield spacer
     
 ### tests
 
