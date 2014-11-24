6645820d571fc23e369d46b30d23e3295b858208
mostlymark <marknye@clubofhumanbeings.com>
2014-11-24 00:03:40 +0000

    improve user edit function

--- ifcb2/dashboard/admin/services.js
+++ ifcb2/dashboard/admin/services.js
@@ -34,17 +34,6 @@ ifcbAdmin.service('InstrumentService', ['Restangular', function (Restangular) {
 
 }]);
 
-ifcbAdmin.service('UserService', ['Restangular', function (Restangular) {
-
-    var baseUsers = Restangular.all('users');this.list = baseUsers.getList();
-    this.post = baseUsers.post;
-
-    this.new = function() {
-        return {first_name:'', last_name:'', email:'', is_enabled:true, edit:true}
-    }
-
-}]);
-
 
 ifcbAdmin.service('RoleService', ['Restangular', function (Restangular) {
 
