858f7d85ac4ad4b982946a8378053f1f2a05989a
Joe Futrelle <joefutrelle@gmail.com>
2015-01-09 16:03:52 +0000

    metrics are now in flot an in a jquery plugin

---
+++ ifcb2/dashboard/static/metrics.js
@@ -0,0 +1,31 @@
+(function($) {
+    $.fn.extend({
+	metric: function(data, key, label) {
+            return this.each(function() {
+                var $this = $(this);
+		var series = [];
+		var minDate = Date.parse(data[0].date);
+		var maxDate = minDate;
+		$.each(data,function(ix,dm) {
+		    var d = Date.parse(dm.date);
+		    if(d > maxDate) {
+			maxDate = d;
+		    }
+		    series.push([d, dm[key]]);
+		});
+		$.plot($this,[series],{
+		    xaxis: {
+			mode: 'time',
+			timeformat: '%y-%m-%d %H:%M',
+			min: minDate,
+			max: maxDate
+		    },
+		    lines: {
+			show: true,
+			steps: true
+		    }
+		});
+	    });
+	}
+    });//$.fn.extend
+})(jQuery);
