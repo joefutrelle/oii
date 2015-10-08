cd8cd81b6fabe2a33d093b3a27f3bf17a236e48e
Joe Futrelle <joefutrelle@gmail.com>
2015-10-08 10:50:45 -0400

    Tagging is now time series specific add/remove/cloud/search all implemented

--- ifcb2/tagging.py
+++ ifcb2/tagging.py
@@ -4,8 +4,9 @@ from sqlalchemy import func
 from oii.ifcb2.orm import Bin, BinTag
 
 class Tagging(object):
-    def __init__(self, session):
+    def __init__(self, session, ts_label):
         self.session = session
+        self.ts_label = ts_label
     def _commit(self, commit):
         if not commit:
             return
@@ -13,20 +14,27 @@ class Tagging(object):
             self.session.commit()
         except IntegrityError:
             self.session.rollback()
-    def tag(self, b, tag, commit=True):
+    def add_tag(self, b, tag, commit=True):
         """add a tag to a bin."""
         # head off duplicates
         if tag not in b.tags:
             b.tags.append(tag)
             self._commit(commit)
-    def untag(self, b, tag, commit=True):
+    def remove_tag(self, b, tag, commit=True):
         """remove a tag from a bin"""
         b.tags.remove(tag)
         self._commit(commit)
-    def tag_cloud(self, ts_label):
+    def tag_cloud(self):
         """get a dict of tags and frequency for a given time series"""
         rows = self.session.query(BinTag.tag, func.count(BinTag.tag)).\
             join(Bin).\
-            filter(Bin.ts_label.like(ts_label)).\
+            filter(Bin.ts_label.like(self.ts_label)).\
             group_by(BinTag.tag, Bin.ts_label)
         return dict(list(rows))
+    def search_tags_all(self, tag_names):
+        q = self.session.query(Bin).\
+            filter(Bin.ts_label.like(self.ts_label))
+        for tag_name in tag_names:
+            q = q.filter(Bin.tags.contains(tag_name))
+        q = q.order_by(Bin.sample_time.desc())
+        return q
