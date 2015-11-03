f29cc89e11e383d13bb4f1238ec7adf50413fcaf
Joe Futrelle <joefutrelle@gmail.com>
2015-11-03 12:15:01 -0500

    feed now accepts tag kw param in constructor and queries respect it

--- ifcb2/feed.py
+++ ifcb2/feed.py
@@ -5,6 +5,8 @@ from sqlalchemy import and_, or_, not_, desc, func, cast, Numeric
 from oii.times import utcdtnow, datetime2utcdatetime
 from oii.ifcb2.orm import Bin, File
 
+from oii.ifcb2.tagging import normalize_tag
+
 def _time_range_params(start_time=None, end_time=None):
     if start_time is None:
         start_time = datetime.utcfromtimestamp(0)
@@ -14,25 +16,27 @@ def _time_range_params(start_time=None, end_time=None):
 
 class Feed(object):
     """use with with"""
-    def __init__(self, session, ts_label):
+    def __init__(self, session, ts_label, tag=None):
         self.session = session
         self.ts_label = ts_label
+        self.tag = tag
     def __enter__(self):
         return self
     def __exit__(self,type,value,traceback):
         pass
+    def _with_tag(self, q):
+        if self.tag is not None:
+            q = q.filter(Bin.tags.contains(normalize_tag(self.tag)))
+        return q
     def _ts_query(self, start_time=None, end_time=None, include_skip=False):
         start_time, end_time = _time_range_params(start_time, end_time)
-        # FIXME not sure how to do boolean value tests right
-        if include_skip:
-            return self.session.query(Bin).filter(and_(\
-                Bin.ts_label==self.ts_label,\
-                Bin.sample_time >= start_time, Bin.sample_time <= end_time))
-        else:
-            return self.session.query(Bin).filter(and_(\
-                Bin.ts_label==self.ts_label,\
-                Bin.sample_time >= start_time, Bin.sample_time <= end_time,\
-                ~Bin.skip))
+        q = self.session.query(Bin).\
+            filter(Bin.ts_label==self.ts_label).\
+            filter(and_(Bin.sample_time >= start_time, Bin.sample_time <= end_time))
+        q = self._with_tag(q)
+        if not include_skip:
+            q = q.filter(~Bin.skip)
+        return q
     def time_range(self, start_time=None, end_time=None):
         """all bins in a given time range"""
         return self._ts_query(start_time, end_time).\
@@ -44,17 +48,23 @@ class Feed(object):
     def daily_data_volume(self, start_time=None, end_time=None):
         """data volume in GB per day over the given time range"""
         start_time, end_time = _time_range_params(start_time, end_time)
-        return self.session.query(cast(func.sum(File.length) / 1073741824.0, Numeric(6,2)), func.count(File.id) / 3, func.DATE(Bin.sample_time)).\
+        q = self.session.query(cast(func.sum(File.length) / 1073741824.0, Numeric(6,2)), func.count(File.id) / 3, func.DATE(Bin.sample_time)).\
             filter(and_(Bin.ts_label==self.ts_label, Bin.sample_time >= start_time, Bin.sample_time <= end_time, ~Bin.skip)).\
-            filter(Bin.id==File.bin_id).\
-            group_by(func.DATE(Bin.sample_time)).\
+            filter(Bin.id==File.bin_id)
+        q = self._with_tag(q)
+        q = q.group_by(func.DATE(Bin.sample_time)).\
             order_by(func.DATE(Bin.sample_time))
+        return q
     def total_data_volume(self):
-        return self.session.query(func.sum(File.length)).join(Bin).\
-            filter(Bin.ts_label==self.ts_label).scalar()
+        q = self.session.query(func.sum(File.length)).join(Bin).\
+            filter(Bin.ts_label==self.ts_label)
+        q = self._with_tag(q)
+        return q.scalar()
     def total_bins(self):
-        return self.session.query(func.count(Bin.id)).\
-            filter(Bin.ts_label==self.ts_label).scalar()
+        q = self.session.query(func.count(Bin.id)).\
+            filter(Bin.ts_label==self.ts_label)
+        q = self._with_tag(q)
+        return q.scalar()
     def nearest(self,n=1,timestamp=None):
         """nearest bin. timestamp must be a utc datetime, defaults to now"""
         if timestamp is None:
@@ -78,24 +88,28 @@ class Feed(object):
             limit(n)
     def after(self,bin_lid,n=1):
         """n bins after a given one"""
-        return self.session.query(Bin).\
+        q = self.session.query(Bin).\
             filter(Bin.ts_label==self.ts_label).\
-            filter(~Bin.skip).\
-            filter(Bin.sample_time > self.session.query(Bin.sample_time).\
+            filter(~Bin.skip)
+        q = self._with_tag(q)
+        q = q.filter(Bin.sample_time > self.session.query(Bin.sample_time).\
                    filter(Bin.lid==bin_lid).\
                    as_scalar()).\
             order_by(Bin.sample_time).\
             limit(n)
+        return q
     def before(self,bin_lid,n=1):
         """n bins before a given one"""
-        return self.session.query(Bin).\
+        q = self.session.query(Bin).\
             filter(Bin.ts_label==self.ts_label).\
-            filter(~Bin.skip).\
-            filter(Bin.sample_time < self.session.query(Bin.sample_time).\
+            filter(~Bin.skip)
+        q = self._with_tag(q)
+        q = q.filter(Bin.sample_time < self.session.query(Bin.sample_time).\
                    filter(Bin.lid==bin_lid).\
                    as_scalar()).\
             order_by(desc(Bin.sample_time)).\
             limit(n)
+        return q
     ## metrics
     def elapsed(self,timestamp=None):
         """time elapsed since latest bin at the given time (default now) (utc datetime).
