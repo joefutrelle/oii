9aa17e1f7c1ad7cdecfa36b0cdb09c77c74de961
Joe Futrelle <joefutrelle@gmail.com>
2015-12-24 11:01:31 -0500

    refactoring and checkpointing
    added imemoize decorator for instance functions

--- ifcb2/features/__init__.py
+++ ifcb2/features/__init__.py
@@ -1,21 +1,80 @@
-BLOB_IMAGE='blob'
-BOUNDING_BOX='bounding_box'
-PIXELS='pixels'
-PERIMETER_IMAGE='perimeter_image'
-CONVEX_HULL_IMAGE='convex_hull_image'
-ROTATED_BLOB_IMAGE='rotated_blob_image'
+import numpy as np
 
-AREA='area'
-EQUIV_DIAMETER='equiv_diameter'
-EXTENT='extent'
-CONVEX_AREA='convex_area'
-CONVEX_PERIMETER='convex_perimeter'
-FERET_DIAMETER='feret_diameter'
-MINOR_AXIS_LENGTH='minor_axis_length'
-MAJOR_AXIS_LENGTH='major_axis_length'
-ECCENTRICITY='eccentricity'
-ORIENTATION='orientation'
-PERIMETER='perimeter'
-SOLIDITY='solidity'
+from skimage.morphology import convex_hull_image
 
+from oii.utils import imemoize
 
+from oii.ifcb2.features.segmentation import segment_roi
+from oii.ifcb2.features.blobs import find_blobs, rotate_blob
+from oii.ifcb2.features.blob_geometry import equiv_diameter, ellipse_properties
+
+class Blob(object):
+    def __init__(self,blob_image):
+        self.image = np.array(blob_image).astype(np.bool)
+    @property
+    @imemoize
+    def convex_hull_image(self):
+        return convex_hull_image(self.image)
+    @property
+    @imemoize
+    def area(self):
+        return np.sum(self.image)
+    @property
+    @imemoize
+    def equiv_diameter(self):
+        return equiv_diameter(self.area)
+    @property
+    @imemoize
+    def extent(self):
+        return float(self.area) / self.image.size
+    @property
+    @imemoize
+    def convex_area(self):
+        return np.sum(self.convex_hull_image)
+    @property
+    @imemoize
+    def feret_diameter(self):
+        return np.max(self.convex_hull_image.flatten())
+    @property
+    @imemoize
+    def ellipse_properties(self):
+        # major axis length, minor axis length, eccentricity, orientation
+        return ellipse_properties(self.image)
+        maj_axis, min_axis, ecc, orientation = ellipse_parameters(B)
+    @property
+    @imemoize
+    def major_axis_length(self):
+        return self.ellipse_properties[0]
+    @property
+    @imemoize
+    def minor_axis_length(self):
+        return self.ellipse_properties[1]
+    @property
+    @imemoize
+    def eccentricity(self):
+        return self.ellipse_properties[2]
+    @property
+    @imemoize
+    def orientation(self):
+        return self.ellipse_properties[3]
+    @property
+    @imemoize
+    def solidity(self):
+        return float(self.area) / self.convex_area
+    @property
+    @imemoize
+    def rotated_image(self):
+        return rotate_blob(self.image, self.orientation)
+        
+class Roi(object):
+    def __init__(self,roi_image):
+        self.image = np.array(roi_image).astype(np.uint8)
+    @property
+    @imemoize
+    def blobs_image(self):
+        return segment_roi(self.image)
+    @property
+    @imemoize
+    def blobs(self):
+        labeled, bboxes, blobs = find_blobs(self.blobs_image)
+        return [Blob(B) for B in blobs]
--- utils.py
+++ utils.py
@@ -20,6 +20,7 @@ from tempfile import mkdtemp
 from contextlib import contextmanager
 import shutil
 import random
+from functools import partial
 
 genid_prev_id_tl = Lock()
 genid_prev_id = None
@@ -92,6 +93,42 @@ def memoize(fn,ttl=31557600,ignore_exceptions=False,key=None):
             exp[args_key] = now + ttl
         return cache[args_key]
     return inner
+
+class imemoize(object):
+    """cache the return value of a method
+    
+    This class is meant to be used as a decorator of methods. The return value
+    from a given method invocation will be cached on the instance whose method
+    was invoked. All arguments passed to a method decorated with memoize must
+    be hashable.
+    
+    If a memoized method is invoked directly on its class the result will not
+    be cached. Instead the method will be invoked like a static method:
+    class Obj(object):
+        @memoize
+        def add_to(self, arg):
+            return self + arg
+    Obj.add_to(1) # not enough arguments
+    Obj.add_to(1, 2) # returns 3, result is not cached
+    """
+    def __init__(self, func):
+        self.func = func
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self.func
+        return partial(self, obj)
+    def __call__(self, *args, **kw):
+        obj = args[0]
+        try:
+            cache = obj.__cache
+        except AttributeError:
+            cache = obj.__cache = {}
+        key = (self.func, args[1:], frozenset(kw.items()))
+        try:
+            res = cache[key]
+        except KeyError:
+            res = cache[key] = self.func(*args, **kw)
+        return res
 
 def retry(ExceptionToCheck, tries=4, delay=3, backoff=2, logger=None):
     """Retry calling the decorated function using an exponential backoff.
