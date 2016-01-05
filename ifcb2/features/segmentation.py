- refactored invmoments to remove unnecessary computation and complexity - fixed bug where texture stats were not being computed on contrast-enhanced pixel values - modified texture stats algorithm to use floating point contrast enhancement instead of 8-bit integer arithmetic, bringing results into line with MATLAB implementation

ifcb2/features/segmentation.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 from scipy.cluster.vq import kmeans2
-
+from scipy.ndimage.morphology import binary_fill_holes
 from skimage.morphology import binary_closing, binary_dilation, remove_small_objects
 
 from oii.ifcb2.features.phasecong import phasecong_Mm
-from oii.ifcb2.features.morphology import SE2, SE3, hysthresh, bwmorph_thin, fill_holes
+from oii.ifcb2.features.morphology import SE2, SE3, hysthresh, bwmorph_thin
 
 # parameters
 HT_T1, HT_T2 = 0.2, 0.1
@@ -39,7 +39,7 @@ def segment_roi(roi):
     # step 8. thinning
     B = bwmorph_thin(B,3)
     # step 9. fill holes
-    B = fill_holes(B)
+    B = binary_fill_holes(B)
     # step 10. remove blobs smaller than BLOB_MIN
     B = remove_small_objects(B,BLOB_MIN,connectivity=2)
     # done.

ifcb2/features/texture.py
@@ -1,13 +1,15 @@
 import numpy as np
 from skimage.exposure import rescale_intensity, histogram
 from scipy.stats import moment
-
-def enhance_contrast(image):
-    p1, p99 = np.percentile(image, (1,99))
-    return rescale_intensity(image)
     
 def masked_pixels(image,mask):
     return image[np.where(mask)]
+    
+def texture_pixels(image,mask):
+    I = image.astype(np.float)
+    p1, p99 = np.percentile(image,(1,99))
+    E = rescale_intensity(I, in_range=(p1,p99)) * 255
+    return E[np.where(mask)]
     
 def statxture(pixels):
     """computes a variety of texture stats from